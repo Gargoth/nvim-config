d97266788ac629a7570ecea29bb8ef3d07c9c17b
Christian Jay F. Rosales <gargoth13@gmail.com>
2023-06-03 15:54:14 +0800

    chore: Added TODO

--- lua/init.lua
+++ lua/init.lua
@@ -4,3 +4,13 @@ require("plugins")
 require("lsp")
 
 vim.cmd [[colorscheme catppuccin]]
+
+-- TODO: Merge branch to main
+-- TODO: Set up lspsaga.nvim
+-- TODO: Tab Mappings / Harpoon
+-- TODO: Code Completions
+-- TODO: Code Actions with null.ls
+-- TODO: Todo plugin mappings
+-- TODO: Telescope plugin mappings
+-- TODO: which-key plugin mappings
+-- TODO: Comment Line mapping
