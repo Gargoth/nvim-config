fc78bfc6f3b69c76472195d7ece70f18bbd73a30
Christian Jay F. Rosales <gargoth13@gmail.com>
2023-07-08 20:49:41 +0800

    Added stuff

--- lua/init.lua
+++ lua/init.lua
@@ -6,3 +6,9 @@ require("lsp")
 
 -- Set colorscheme
 vim.cmd([[colorscheme catppuccin]])
+
+-- Neovide
+if vim.g.neovide then
+    vim.o.guifont = "JetBrains Mono NF:h10"
+    vim.g.neovide_scale_factor = 0.85
+end
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,7 +11,7 @@ require("lazy").setup({
             "folke/neodev.nvim",
             opts = {
                 library = { plugins = { "nvim-dap-ui" }, types = true },
-            } 
+            }
         },
 
         {
@@ -135,7 +135,7 @@ require("lazy").setup({
         {
             "rcarriga/nvim-dap-ui",
             opts = {},
-            dependencies ={
+            dependencies = {
                 "mfussenegger/nvim-dap",
             }
         },
@@ -145,11 +145,7 @@ require("lazy").setup({
             name = "catppuccin",
             opts = {
                 flavour = "macchiato", -- latte, frappe, macchiato, mocha
-                background = {
-                    light = "latte",
-                    dark = "macchiato",
-                },
-                transparent_background = true,
+                transparent_background = false,
                 integrations = {
                     gitsigns = true,
                     harpoon = true,
@@ -306,6 +302,43 @@ require("lazy").setup({
         {
             "brenoprata10/nvim-highlight-colors",
             opts = {}
+        },
+
+        {
+            "andweeb/presence.nvim",
+            opts = {
+                -- General options
+                auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
+                neovim_image_text   = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
+                main_image          = "neovim",                   -- Main image display (either "neovim" or "file")
+                client_id           = "793271441293967371",       -- Use your own Discord application client id (not recommended)
+                log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+                debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+                enable_line_number  = false,                      -- Displays the current line number instead of the current project
+                blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+                buttons             = true,                       -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
+                file_assets         = {},                         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
+                show_time           = true,                       -- Show the timer
+
+                -- Rich Presence text options
+                editing_text        = "Editing %s",         -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+                file_explorer_text  = "Browsing %s",        -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+                git_commit_text     = "Committing changes", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+                plugin_manager_text = "Managing plugins",   -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+                reading_text        = "Reading %s",         -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+                workspace_text      = "Working on %s",      -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+                line_number_text    = "Line %s out of %s",  -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+            }
+        },
+
+        {
+            "LhKipp/nvim-nu",
+            build = ":TSInstall nu",
+            opts = {},
+        },
+
+        {
+            "rhysd/conflict-marker.vim",
         }
     },
 
@@ -316,7 +349,7 @@ require("lazy").setup({
 })
 
 -- Tresitter setup
-require 'nvim-treesitter.install'.compilers = {'clang'}
+require 'nvim-treesitter.install'.compilers = { 'clang' }
 
 -- Wilder setup
 local wilder = require('wilder')
