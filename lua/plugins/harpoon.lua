a84f64b469fbf6d1aa5732c3da9083daaa8908c4
CJ Rosales <gargoth13@gmail.com>
2024-04-25 21:04:09 +0800

    feat: Add nushell support

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -15,7 +15,7 @@ return {
       {
         "<leader>H",
         function()
-          require("harpoon"):list():append()
+          require("harpoon"):list():add()
         end,
         desc = "Harpoon file",
       },
