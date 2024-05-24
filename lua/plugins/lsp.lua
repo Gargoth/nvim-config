044320d6afd3bf536564c802176e7c9cbfdb58c5
CJ Rosales <gargoth13@gmail.com>
2024-05-24 22:13:11 +0800

    feat: Properly add svelte

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,19 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        svelte = {},
-      },
+  "neovim/nvim-lspconfig",
+  opts = {
+    setup = {
+      svelte = function(_, opts)
+        LazyVim.lsp.on_attach(function(client, buffer)
+          if client.name == "svelte" then
+            vim.api.nvim_create_autocmd("BufWritePost", {
+              pattern = { "*.js", "*.ts" },
+              callback = function(ctx)
+                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+              end,
+            })
+          end
+        end)
+      end,
     },
   },
 }
