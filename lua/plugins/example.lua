bd9d98a9cb3ef2083b578e274f88b4fb5db8ed87
CJ Rosales <gargoth13@gmail.com>
2024-05-24 12:33:16 +0800

    feat: Updates

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -193,12 +193,12 @@ return {
 
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   keys = function()
+  --     return {}
+  --   end,
+  -- },
   -- then: setup supertab in cmp
   {
     "hrsh7th/nvim-cmp",
