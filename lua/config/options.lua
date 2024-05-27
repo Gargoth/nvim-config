aebbbf29caf409bc5d169aa183347485bbcc8191
CJ Rosales <gargoth13@gmail.com>
2024-05-27 23:14:09 +0800

    fbasdjnkmfldsa

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.autoformat = false
+local g = vim.g
+local wo = vim.wo
+local bo = vim.bo
+
+local TAB_WIDTH = 4
+
+bo.tabstop = TAB_WIDTH
+bo.shiftwidth = TAB_WIDTH
+bo.expandtab = true
+g.autoformat = false
