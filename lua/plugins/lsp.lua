27bf75e409cb97200724c46dea9bcaf9770994c1
CJ Rosales <gargoth13@gmail.com>
2024-06-08 20:00:34 +0800

    feat: Use native lazyvim svelte

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,19 +6,5 @@ return {
       update_in_insert = false,
       virtual_text = false,
     },
-    setup = {
-      svelte = function(_, opts)
-        LazyVim.lsp.on_attach(function(client, buffer)
-          if client.name == "svelte" then
-            vim.api.nvim_create_autocmd("BufWritePost", {
-              pattern = { "*.js", "*.ts" },
-              callback = function(ctx)
-                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-              end,
-            })
-          end
-        end)
-      end,
-    },
   },
 }
