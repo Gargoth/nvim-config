1ccb6b5382212a3e43ddb1cb38bfb0cb23e68317
Christian Jay F. Rosales <gargoth13@gmail.com>
2024-07-03 17:40:34 +0800

    feat: Added keymap for toggling autoformat

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -11,10 +11,23 @@ return {
         mode = '',
         desc = '[C]ode [F]ormat buffer',
       },
+      {
+        '<leader>tf',
+        function()
+          vim.g.autoformat = not vim.g.autoformat
+          print('Autoformat on save toggled to', vim.g.autoformat)
+        end,
+        mode = '',
+        desc = '[T]oggle Auto[f]ormat on Save',
+      },
     },
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        if not vim.g.autoformat then
+          return
+        end
+
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
