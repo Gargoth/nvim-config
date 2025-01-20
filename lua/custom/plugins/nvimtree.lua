e05dbb8bbd9caf0ca73aebfe4b0530b9dfaeecce
Christian Jay Rosales <crosales@freelancer.com>
2025-01-21 07:51:01 +0800

    Configure nvim-tree to be centered and floating

--- lua/custom/plugins/nvimtree.lua
+++ lua/custom/plugins/nvimtree.lua
@@ -5,9 +5,41 @@ return {
   dependencies = {
     'nvim-tree/nvim-web-devicons',
   },
-  opts = {},
   keys = {
-    { '\\', ':NvimTreeOpen<CR>', { desc = 'NvimTree open' } },
+    { '\\', ':NvimTreeFindFile<CR>', { desc = 'NvimTree open' } },
     { '|', ':NvimTreeClose<CR>', { desc = 'NvimTree close' } },
+  },
+  opts = {
+    disable_netrw = true,
+    hijack_netrw = true,
+    respect_buf_cwd = true,
+    sync_root_with_cwd = true,
+    view = {
+      relativenumber = true,
+      float = {
+        enable = true,
+        open_win_config = function()
+          local screen_w = vim.opt.columns:get()
+          local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+          local window_w = screen_w * 1/2
+          local window_h = screen_h * 1/2
+          local window_w_int = math.floor(window_w)
+          local window_h_int = math.floor(window_h)
+          local center_x = (screen_w - window_w) / 2
+          local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+          return {
+            border = 'rounded',
+            relative = 'editor',
+            row = center_y,
+            col = center_x,
+            width = window_w_int,
+            height = window_h_int,
+          }
+        end,
+      },
+      width = function()
+        return math.floor(vim.opt.columns:get() * 1/2)
+      end,
+    },
   },
 }
